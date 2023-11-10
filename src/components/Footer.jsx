31506865c68770c18f6321598161e5d2d926dac2
Innocent Josiah Patrick <innocentjosiah57@gmail.com>
2023-11-11 00:19:56 +0100

    finally done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { logo2 } from '../assets';
 function Footer() {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
-      <Stack gap="40px" pt="24px" px="40px">
+      <Stack gap="40px" pt="24px" px="40px" alignItems="center" justifyContent="center">
         <img src={logo2} alt="logo" width="200px" height="40px" />
         <Typography variant="h5" pb="40px" mt="20px">
           Made with ❤ by Innocent Patrick
